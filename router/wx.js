0089c3ea46626a54d3aef2d5960301c62013dee2
admin <qinkangwu@xs1h.com>
2018-07-19 22:41:08 +0800

    asd

--- router/wx.js
+++ router/wx.js
@@ -1,6 +1,6 @@
 const router = require('koa-router')();
 const wx = require('../controllers/wx.js');
 
-router.get('wx',wx.checkTokenUrl);
+router.get('/wx',wx.checkTokenUrl);
 router.post('/',wx.getXmlDataFronWxServer);
 module.exports = router;
