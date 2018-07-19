d8ae7f0826eec81fd700cc05b5d647ec9b19b3f2
admin <qinkangwu@xs1h.com>
2018-07-19 23:14:43 +0800

    asd

--- controllers/wx.js
+++ controllers/wx.js
@@ -3,27 +3,27 @@ const result = require('../lib/result');
 const sha1 = require('js-sha1');
 var hash = sha1.create();
 exports.checkTokenUrl = async ctx=>{
-    console.log(2);
-    let signature = ctx.query.signature || '';
-    let echostr = ctx.query.echostr || '';
-    let timestamp = ctx.query.timestamp || '';
-    let nonce = ctx.query.nonce || '';
-    let token = config.wxCheckUrlToken;
+    ctx.body='hello world';
+    // let signature = ctx.query.signature || '';
+    // let echostr = ctx.query.echostr || '';
+    // let timestamp = ctx.query.timestamp || '';
+    // let nonce = ctx.query.nonce || '';
+    // let token = config.wxCheckUrlToken;
 
-    if(!signature || !echostr || !timestamp || !nonce){
-        ctx.body = result.fail('bad request~',403);
-    }
+    // if(!signature || !echostr || !timestamp || !nonce){
+    //     ctx.body = result.fail('bad request~',403);
+    // }
 
-    let arr = [token,timestamp,nonce].sort();
-    arr.map((res,index)=>{
-        hash.update(res);
-    })
-    let hashcode = hash.hex();
-    if(hashcode == signature){
-        ctx.body = echostr;
-    }else{
-        ctx.body = result.fail('bad request~',403);
-    }
+    // let arr = [token,timestamp,nonce].sort();
+    // arr.map((res,index)=>{
+    //     hash.update(res);
+    // })
+    // let hashcode = hash.hex();
+    // if(hashcode == signature){
+    //     ctx.body = echostr;
+    // }else{
+    //     ctx.body = result.fail('bad request~',403);
+    // }
 }
 
 exports.getXmlDataFronWxServer = async ctx=>{
