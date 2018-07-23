3d67a368f521458cdb76a84de98865b9ddb8b0c4
admin <qinkangwu@xs1h.com>
2018-07-23 17:58:37 +0800

    asd

--- models/wxDataModel.js
+++ models/wxDataModel.js
@@ -3,32 +3,32 @@ const getAccessToken = require('../lib/wxAccessToken');
 let instance = null;
 class WxModel {
     constructor(xmlData){
-        if(!xmlData ) throw Error('xml数据为空');
-        this.xmlObj = xmlData;
         if(!instance) instance = this;
+        if(!xmlData ) throw Error('xml数据为空');
+        instance.xmlObj = xmlData;
         return instance;
     }
     sendText(){
-        let accessToken = getAccessToken.getAccessToken();
+        //console.log(this.xmlObj);   
         return xml.jsonToXml({ 
             xml: { 
-                ToUserName: [ this.xmlObj.FromUserName[0] ],
-                FromUserName: [ this.xmlObj.ToUserName[0] ],
+                ToUserName: [ instance.xmlObj.FromUserName[0] ],
+                FromUserName: [ instance.xmlObj.ToUserName[0] ],
                 CreateTime: [ +new Date ],
                 MsgType: [ 'text' ],
-                Content: [ this.xmlObj.Content[0] ]
+                Content: [ instance.xmlObj.Content[0] ]
             } 
         })
     }
     sendImg(){
         return xml.jsonToXml({
             xml : {
-                ToUserName : [ this.xmlObj.FromUserName[0] ],
-                FromUserName : [ this.xmlObj.ToUserName[0] ],
+                ToUserName : [ instance.xmlObj.FromUserName[0] ],
+                FromUserName : [ instance.xmlObj.ToUserName[0] ],
                 CreateTime: [ +new Date ],
                 MsgType: [ 'image' ],
                 Image : {
-                    MediaId : [ this.xmlObj.MediaId[0] ]
+                    MediaId : [ instance.xmlObj.MediaId[0] ]
                 }
             }
         })
